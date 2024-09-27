032bd12be4f41893e636686277fcd0a063c8de36
jjikky <kok5417@skuniv.ac.kr>
2024-09-27 16:33:31 +0900

    feat: UsersModule에 파일 모듈 및 관계형 퍼시스턴스 모듈 추가 #11

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { FilesModule } from 'src/files/files.module';
+import { RelationalUserPersistenceModule } from './infrastructure/persistence/relational/relational-persistence.module';
+
+const infrastructurePersistenceModule = RelationalUserPersistenceModule;
 
 @Module({
+  imports: [infrastructurePersistenceModule, FilesModule],
+  controllers: [UsersController],
   providers: [UsersService],
+  exports: [UsersService, infrastructurePersistenceModule],
 })
 export class UsersModule {}
